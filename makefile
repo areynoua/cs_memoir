f7017f88c5f930cea3e5453dc81ab4dbb0fd76b0
Alexis Reynouard <areynoua@ulb.ac.be>
2018-07-27 18:33:07 +0200

    Colourful tex log output

--- makefile
+++ makefile
@@ -1,14 +1,20 @@
+SHELL:=/bin/bash
+
 all: main.pdf
 
-main.aux main.bbl: main.tex $(wildcard parts/*.tex) info.bib
-	xelatex main.tex
-	bibtex main.aux
-	xelatex main.tex
+main.aux main.bbl: main.tex $(wildcard parts/*.tex) $(wildcard res/*.tex) info.bib
+	$$(echo X) | xelatex main.tex | ./texout.py ; [ $${PIPESTATUS[1]} -eq 0 ] || ( make clean && false )
+	bibtex main.aux  || ( make clean && false )
+	$$(echo X) | xelatex main.tex | ./texout.py ; [ $${PIPESTATUS[1]} -eq 0 ] || ( make clean && false )
 
-main.pdf: main.tex main.aux main.bbl $(wildcard parts/*.tex)
-	xelatex main.tex
+main.pdf: main.tex main.aux main.bbl $(wildcard parts/*.tex) $(wildcard res/*.tex)
+	@echo -e '\n\n\n--------------------------------------------------------------------------------\n\n\n'
+	$$(echo X) | xelatex main.tex | ./texout.py ; [ $${PIPESTATUS[1]} -eq 0 ] || ( make clean && false )
 
 clean:
 	rm -f main.aux main.bbl main.bib main.log
 
-.PHONY: clean
+test:
+	$$(echo X) | xelatex main.tex | ./texout.py ; [ $${PIPESTATUS[1]} -eq 0 ] || ( make clean && false )
+
+.PHONY: all clean test
---
+++ texout.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+
+from sys import stdin
+
+letters = 'abcdefghijklmnopqrstuvwxyz'
+
+def read() :
+    return stdin.read(1)
+
+def read_nonl() :
+    c = stdin.read(1)
+    while c == '\n' :
+        c = stdin.read(1)
+    return c
+
+def push(c) :
+    name = []
+    ext = []
+    c = read_nonl()
+    while (len(ext) != 4 or c in letters) and c != ')' :
+        if c not in letters :
+            if len(ext) != 0 :
+                name.extend(ext)
+                ext = []
+            if c == '.' :
+                ext = ['.']
+            else :
+                name.append(c)
+        else :
+            if len(ext) not in (0,4) :
+                ext.append(c)
+            else :
+                name.extend(ext)
+                ext = []
+                name.append(c)
+        c = read_nonl()
+    staged = ''.join(name+ext)
+
+    if '/' not in staged or ' ' in staged and c == ')' :
+        message('(' + staged + ')')
+        return None
+    else :
+        print_messages()
+        stack.append(staged)
+
+    return c
+
+def pop(c) :
+    print_messages()
+    stack.pop()
+    return None
+
+def message(c) :
+    messages.append(c)
+    return None
+
+def print_messages() :
+    global messages, last_level_printed
+    msg = ''.join(messages).strip()
+    if len(msg) > 0 and not msg.isspace() :
+        if len(stack) > 0 and last_level_printed != stack[-1] :
+            last_level_printed = stack[-1]
+            print('[94;1m', last_level_printed, '[39;0m:', sep='')
+        msg = msg.replace('\n\n\n','\n\n').replace('\n.\n','.\n')
+        for line in msg.splitlines() :
+            if 'Warning' in line :
+                print('[91;1m', line, '[39;0m', sep='')
+            else :
+                print(line)
+    elif len(stack) > 0 and last_level_printed != stack[-1] and stack[-1][0] == '.' :
+            last_level_printed = stack[-1]
+            print('[94;1m', last_level_printed, '[39;0m:', sep='')
+    messages = []
+
+messages = []
+stack = []
+last_level_printed = ''
+
+actions = {
+        '(' : push,
+        ')' : pop,
+        }
+
+c = read()
+while c != '' :
+    c = actions.get(c, message)(c)
+    while c != '' and c is not None :
+        c = actions.get(c, message)(c)
+    c = read()
+
+print_messages()
