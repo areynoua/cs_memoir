68982d9da7aecab6c103f8e0bc44d109f2ef7a28
Alexis Reynouard <areynoua@ulb.ac.be>
2018-08-10 23:06:44 +0200

    technical

--- vim.sh
+++ vim.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-vim $(sed -n 's/^\s*\\input{\(parts\/[^}]\+\)\(\.tex\)\?}/\1.tex/gp' main.tex)
+vim $(sed -n 's/^\s*\\input{\(parts\/[^}]\+\)\(\.tex\)\?}/\1.tex/gp' main.tex) main.tex .vimrc
